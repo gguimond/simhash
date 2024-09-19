755a0eab705aaaeaeef450d957b4abb6a0e8df3a
Guillaume Guimond <guillaume.guimond@orange.com>
2024-09-19 12:00:29 +0200

    added comment for better understanding & added distance func

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ from itertools import groupby
 import hashlib
 import numpy as np
 
-
-def main():
-    content = 'hello world'
+def simhash(content):
     content = content.lower()
     reg = r'[\w\u4e00-\u9fcc]+'
     content = ''.join(re.findall(reg, content))
@@ -26,17 +24,38 @@ def main():
         print(h)
         print(np.frombuffer(h, dtype='>B'))
         print(np.unpackbits(np.frombuffer(h, dtype='>B')))
+        #gettting binary values
         sums.append(np.unpackbits(np.frombuffer(h, dtype='>B')) * w)
         if len(sums) >= 200:
+            #sum all bits
             sums = [np.sum(sums, 0)]
 
     sums = np.sum(sums, 0)
     print(sums)
     print(sums > count / 2)
     print(np.packbits(sums > count / 2))
-
+    #keep bits with sum > count/2 (more 1s than 0s)
     value = int.from_bytes((np.packbits(sums > count / 2).tobytes()), 'big')
-    print(value)
+    return value
+
+def distance(s1, s2):
+    #diff with xor
+    x = (s1 ^ s2)
+    print(np.unpackbits(np.frombuffer(((1 << 64) -1).to_bytes(8, 'big'), dtype='>B')))
+    print(np.unpackbits(np.frombuffer(x.to_bytes(8, 'big'), dtype='>B')))
+    ans = 0
+    while x:
+        ans += 1
+        #x-1 > switch rightmost 1 bit to 0
+        #increment counter and then redo xor to find next diff bit
+        x &= x - 1
+        print(np.unpackbits(np.frombuffer(x.to_bytes(8, 'big'), dtype='>B')))
+    return ans
+
+def main():    
+    print(distance(simhash('hello world'), simhash('hello world')))
+    print(distance(simhash('hello world'), simhash('hello world2')))
+
 
 if __name__ == "__main__":
     main()
